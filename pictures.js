3e1a83c3808bb872c95a0c514239815f520c210b
swagatasingha40 <swagatasingha40@gmail.com>
2024-07-21 11:19:46 +0530

    first commit

---
+++ pictures.js
@@ -0,0 +1,90 @@
+var theImages = new Array();
+theImages[0] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/MELODY.png?v=1622820115";
+theImages[1] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DH8565_100_E_PREM.png?v=1622820115";
+theImages[2] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DH3228_100_E_PREM.png?v=1622820115";
+theImages[3] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DH0690_200_E_PREM.png?v=1622820115";
+theImages[4] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DD1876_700_E_PREM.png?v=1622820115";
+theImages[5] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DD1870_100_E_PREM.png?v=1622820115";
+theImages[6] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DC9936_100_E_PREM.png?v=1622820115";
+theImages[7] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-DA2728_100_E_PREM.png?v=1622820115";
+theImages[8] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CV9388_100_E_PREM.png?v=1622820115";
+theImages[9] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CU3244_100_E_PREM.png?v=1622820115";
+theImages[10] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CU1726_101_E_PREM.png?v=1622820115";
+theImages[11] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CT8480_001_E_PREM.png?v=1622820115";
+theImages[12] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CT5053_001_E_PREM.png?v=1622820115";
+theImages[13] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CT2552_800_E_PREM.png?v=1622820115";
+theImages[14] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CT0856_700_E_PREM.png?v=1622820115";
+theImages[15] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CT0856_100_E_PREM.png?v=1622820115";
+theImages[16] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CK5666_100_E_PREM.png?v=1622820115";
+theImages[17] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CQ7629_100_E_PREM.png?v=1622820115";
+theImages[18] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CI1184_146_E_PREM.png?v=1622820115";
+theImages[19] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CI1173_400_E_PREM.png?v=1622820115";
+theImages[20] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CD7001_300_E_PREM.png?v=1622820115";
+theImages[21] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CD6578_507_E_PREM.png?v=1622820115";
+theImages[22] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CD4487_100_E_PREM.png?v=1622820115";
+theImages[23] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-CD0461_401_E_PREM.png?v=1622820115";
+theImages[24] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-BV0073_301_E_PREM.png?v=1622820115";
+theImages[25] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-BV0073_300_E_PREM.png?v=1622820115";
+theImages[26] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-AQ0818_148_E_PREM.png?v=1622820115";
+theImages[27] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-AQ0818_100_E_PREM.png?v=1622820114";
+theImages[28] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-AJ4219_400_E_PREM.png?v=1622820115";
+theImages[29] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-AA3834_101_E_PREM.png?v=1622820115";
+theImages[30] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-AA3830_100_E_PREM.png?v=1622820115";
+theImages[31] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-AA3830_002_E_PREM.png?v=1622820115";
+theImages[32] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-555088_402_E_PREM.png?v=1622820115";
+theImages[33] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-555088_140_E_PREM.png?v=1622820115";
+theImages[34] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-555088_105_E_PREM.png?v=1622820115";
+theImages[35] =
+  "https://cdn.shopify.com/s/files/1/0016/3246/9027/files/JORDAN-555088_028_E_PREM.png?v=1622820115";
+
+var j = 0;
+var p = theImages.length;
+var preBuffer = new Array();
+
+for (i = 0; i < p; i++) {
+  preBuffer[i] = new Image();
+  preBuffer[i].src = theImages[i];
+}
+var whichImage = Math.round(Math.random() * (p - 1));
+
+function showImage() {
+  document.getElementById("shoeimg").src = theImages[whichImage];
+}
+
+//call the function
+showImage();
